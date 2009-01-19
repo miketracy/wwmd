ec225686efce9106c527faae298cfc8dbc9e1337
Michael Tracy <mike.tracy@gmail.com>
2009-01-19 15:21:04 -0600

    code.class == Fixnum

--- page/auth.rb
+++ page/auth.rb
@@ -16,7 +16,7 @@ module WWMD
 
     # check if this request requires NTLM
     def ntlm?
-      return false if self.code != "401"
+      return false if self.code != 401
       count = 0
       self.header_data.each do |i|
         if i[0] =~ /www-authenticate/i
@@ -28,7 +28,7 @@ module WWMD
 
     # does this request have an authenticate header?
     def auth?
-      return false if self.code != "401"
+      return false if self.code != 401
       count = 0
       self.header_data.each do |i|
         if i[0] =~ /www-authenticate/i
