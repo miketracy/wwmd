d49caf276e72884fbe825ce9131cb92fc2be3807
Michael Tracy <mike.tracy@gmail.com>
2009-06-12 13:16:33 -0500

    move things around

--- lib/wwmd/viewstate.rb
+++ lib/wwmd/viewstate.rb
@@ -1,13 +1,13 @@
+require 'rubygems'
+require 'nokogiri'
+require 'rexml/document'
+require 'htmlentities'
 require 'wwmd/viewstate/viewstate_utils'
 module WWMD
   class ViewState
     include ViewStateUtils
   end
 end
-require 'rubygems'
-require 'nokogiri'
-require 'rexml/document'
-require 'htmlentities'
 Dir.glob(::File.join(::File.dirname(__FILE__),"mixins*.rb")).each { |rb| require rb }
 require 'wwmd/viewstate/viewstate'
 require 'wwmd/viewstate/viewstate_types'
