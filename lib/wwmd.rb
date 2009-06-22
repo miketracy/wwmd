29e5b60c388f874707287141d72c25ddd3951eff
Michael Tracy <mike.tracy@gmail.com>
2009-06-22 17:20:51 -0500

    version bump

--- lib/wwmd.rb
+++ lib/wwmd.rb
@@ -15,7 +15,7 @@ require 'rexml/document'
 module WWMD
 
   # :stopdoc:
-  VERSION = "0.2.16"
+  VERSION = "0.2.17"
   PARSER = :nokogiri  # :nokogiri || :hpricot
   LIBPATH = ::File.expand_path(::File.dirname(__FILE__)) + ::File::SEPARATOR
   PATH = ::File.dirname(LIBPATH) + ::File::SEPARATOR
